7e78919ad3c85d49b6aa93550ac40b2fe94f29f2
Khant Wai Yan <52145576+WaiYan70@users.noreply.github.com>
2024-03-08 21:08:21 +0630

    Finish the models and implementing middleware

---
+++ README.md
@@ -0,0 +1,6 @@
+In this creating API, I use these package as below 
+- express as Node JS web framework
+- mongodb as DataBase
+- mongoose as mongodb driver
+
+For API testing tool, I utilize the "Insomnia"
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
 const app = express();
+
+// why use Middleware becasue JSON is not passed through node js by default
+// So, Middleware is used to configure it
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
@@ -8,6 +13,11 @@ app.listen(3000, () => {
 
 app.get('/', (req, res)=> {
     res.send('Lets Create a Simple CRUD Node API Server!..');
+});
+
+app.post('/api/products', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
 });
 
 mongoose.connect("mongodb+srv://khantwaiyan11:WXb25x87x4fJNBxz@simplebackenddb.wvmxut6.mongodb.net/Simple-CRUD-Node-API?retryWrites=true&w=majority&appName=SimpleBackendDB").then(() => {
