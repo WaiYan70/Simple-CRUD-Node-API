5302432a5901592be3ea0ee7bb6b5615a2757f47
Khant Wai Yan <52145576+WaiYan70@users.noreply.github.com>
2024-03-09 22:34:53 +0630

    Finish Create and Read API functions

--- index.js
+++ index.js
@@ -1,27 +1,54 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const Product = require('./models/product.model.js');
+
 const app = express();
 
 // why use Middleware becasue JSON is not passed through node js by default
 // So, Middleware is used to configure it
 app.use(express.json());
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
-
 app.get('/', (req, res)=> {
     res.send('Lets Create a Simple CRUD Node API Server!..');
 });
 
-app.post('/api/products', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
+// Read API, to view all created products
+app.get('/api/products', async (req, res) => {
+    try{
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message : error.message});
+    }
+});
+
+// Read API, to view the specifi create product
+app.get('/api/products/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message : error.message});
+    }
+});
+
+// Create API, to create new products with new data
+app.post('/api/products', async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error){
+        res.status(500).json({message : error.message})
+    }
 });
 
 mongoose.connect("mongodb+srv://khantwaiyan11:WXb25x87x4fJNBxz@simplebackenddb.wvmxut6.mongodb.net/Simple-CRUD-Node-API?retryWrites=true&w=majority&appName=SimpleBackendDB").then(() => {
     console.log("Connected to DataBase!");
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
 }).catch(() =>{
     console.log("Connection Failed");
 })
